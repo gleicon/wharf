5a5420a83e6ec23ad23730efff999faed1796281
Juliano Martinez <juliano@falaserio.org>
2012-03-11 16:51:11 -0300

    adding sample debian template

---
+++ src/lib/cfengine.py
@@ -0,0 +1,23 @@
+class Promise(object):
+    def create(self):
+        pass
+
+    def write(self):
+        pass
+
+class Bundle(object):
+    def agent(self):
+        pass
+
+class Action(object):
+    def vars(self):
+        pass
+
+    def files(self):
+        pass
+
+    def packages(self):
+        pass
+
+    def commands(self):
+        pass
---
+++ src/lib/wharf.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+import os
+import sys
+import redis
+import cfengine
+import subprocess
+import ConfigParser
+
+class WharfConfig(object):
+    config_file = '/etc/wharf/wharf.config'
+
+    def __init__(self):
+        self.config = ConfigParser.RawConfigParser()
+        self.config.readfp('/etc/wharf/wharf.config')
+        self.__setup__()
+
+    def get(self, *args, **kwargs)
+        return self.config.get(*args, **kwargs)
+
+class Templates(object):
+    def __init__(self):
+        pass
+
+class Provisioning(object):
+    def __init__(self):
+        self.config = WharfConfig()
+
+    def bootstrap(self, **kwargs):
+        base_storage = self.config.get('Provisioning', 'base_storage')
+        template = Template()
+        template.deploy(kwargs[disto], kwargs[name])
+        lxc_config = ManageConfig()
+        lxc_config.add('lxc.network.type', 'veth')
+        lxc_config.add('lxc.network.flags', 'up')
+        lxc_config.add('lxc.network.hwaddr', kwargs[hwaddr])
+        lxc_config.add('lxc.network.mtu', '1500')
+        lxc_config.add('lxc.network.name', 'eth0')
+        lxc_config.add('lxc.network.ipv4', kwargs[ipv4])
+        action = cfengine.Action()
+        action.packages('add', ['iproute2', 'bind-utils'])
+        action.files('server_copy', {'/etc/resolv.conf', {'from': '$(masterfiles)/etc/resolv.conf'}})
+        bundle = cfengine.Bundle('config_%s' % name)
+        bundle.agent.add(bundle)
+        promise = cfengine.Promise('config_%s' % name)
+        promise.add(bundle)
+        promise.write()
+
+    def quota(self, **kwargs):
+        pass
+
+    def device(self):
+        pass
+
+class ManageConfig(object):
+    def __init__(self):
+        self.config = WharfConfig()
+
+    def add(self, namespace, value):
+        pass
+
+    def get(self, namespace, value):
+        pass
+
+    def list(self):
+        pass
+
+    def keys(self, namespace=None):
+        pass
+
+    def load(self, file):
+        pass
+
+    def save(self, file):
+        pass
+
+    def write(self, file=None):
+        pass
+
